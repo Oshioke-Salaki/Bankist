3c8d911608b2dcf584a6075be5c56ade3885abfb
Oshioke-Salaki <salaki1902@gmail.com>
2022-11-27 11:04:31 -0600

    Updated

--- script.js
+++ script.js
@@ -18,11 +18,15 @@ const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
 const dotContainer = document.querySelector('.dots');
 
+window.addEventListener('beforeunload', e => {
+    e.preventDefault();
+    e.returnValue = 'Are you sure you want to leave';
+});
+
 // Modal window
 const openModal = function(e) {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-
     e.preventDefault();
 };
 
